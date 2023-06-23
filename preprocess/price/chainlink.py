ba1acc62d706b75aaedb0cbe40a6577efe6a63a1
Evan-Kim2028 <ekcopersonal@gmail.com>
2023-06-23 13:11:44 -0400

    refactor(preprocess): rename files

---
+++ preprocess/price/chainlink.py
@@ -0,0 +1,28 @@
+import polars as pl
+import os
+
+pl.Config.set_fmt_str_lengths(200)
+
+
+if not os.path.exists('data/price'):
+    os.makedirs('data/price')
+
+
+price_filepaths = 'data/prices_raw/chainlink-prices-subgraph/*.parquet'
+
+
+df = (
+    pl.scan_parquet(price_filepaths)
+).collect()
+
+# sort by timestamp
+df = df.sort('timestamp')
+
+# convert timestamp to datetime
+df = df.with_columns(
+    pl.from_epoch("timestamp"),
+    price= pl.col("price") / 10 ** 6,
+)
+
+# save df
+df.write_parquet('data/price/chainlink_eth_price.parquet')
